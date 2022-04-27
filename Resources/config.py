cc70fbb6e79cc6f1b692f54b3c2d4891bdcb1a48
RomanR <rochevftw@gmail.com>
2022-04-27 19:24:56 +0300

    Automatic download and update of chromedriver

--- Resources/config.py
+++ Resources/config.py
@@ -3,5 +3,6 @@ import os
 """Базовые переменные для работы тестов"""
 chrome_driver_path = os.path.join('Drivers', 'chromedriver.exe')
 html_report_folder = 'HTML Report'
+webdrivers_folder = 'Drivers'
 url_yandex_search = "https://yandex.ru"
 url_yandex_images = "https://yandex.ru/images/"
--- conftest.py
+++ conftest.py
@@ -1,15 +1,27 @@
 from Resources import config
 import pytest
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 
+@pytest.fixture(scope='session')
+def get_latest_driver():
+    """
+    Автоматически скачивает актуальную версию chromedriver
+
+    :return: путь к драйверу
+    """
+    d_path = ChromeDriverManager(path=config.webdrivers_folder).install()
+    return d_path
+
+
 @pytest.fixture()
-def init_driver(request):
+def init_driver(get_latest_driver, request):
     """
     Создает Webdriver для использования в тестах и закрывает его по их завершению
     """
-    service = Service(config.chrome_driver_path)
+    service = Service(get_latest_driver)
     driver = webdriver.Chrome(service=service)
     request.cls.driver = driver
     yield
